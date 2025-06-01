caaa82de5883265e3ec53156d51cd9985d4b9a83
Simon Hill <simon.hill@adailycanof.co.uk>
2025-06-01 11:11:45 +0100

    working tf deployment

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -9,10 +9,10 @@ output "alb_zone_id" {
   value       = aws_lb.main.zone_id
 }
 
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.app.repository_url
-}
+# output "ecr_repository_url" {
+#   description = "URL of the ECR repository"
+#   value       = aws_ecr_repository.app.repository_url
+# }
 
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
